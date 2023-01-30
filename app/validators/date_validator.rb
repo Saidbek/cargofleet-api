75467b933a5c5b571ca1110bf57f0be93a90b1ec
Said Kaldybaev <said.kaldybaev@gmail.com>
2023-01-29 21:57:12 -0800

    added date validator

---
+++ app/validators/date_validator.rb
@@ -0,0 +1,7 @@
+class DateValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    if value && value <= Date.today
+      record.errors[attribute] << (options[:message] || "must be a future date")
+    end
+  end
+end
