992cba938aaae135362fe0f33032adccb7045f94
Said Kaldybaev <said.kaldybaev@gmail.com>
2024-01-31 19:57:12 -0800

    add auth token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
   rescue_from Apipie::ParamError do |e|
     render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  private
+
+  def authenticate
+    token = request.headers['Authorization']
+    unless token == ENV.fetch('AUTHORIZATION_TOKEN', 'Zb84MzAROCrhmF6t')
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
   end
 end
